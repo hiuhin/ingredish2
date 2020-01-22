f535edd3f39942dc7007a8fd754c9c2419c5eb49
Anne Wong <hiuhin@gmail.com>
2020-01-22 12:02:06 -0800

    Add Modal

--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -1,6 +1,9 @@
 import { connect } from "react-redux";
+import React from 'react';
 import { signup } from "../../actions/session_actions";
 import SignupForm from "./signup_form";
+import { openModal, closeModal } from "../../actions/modal_actions";
+import '../modal/modal.scss';
 
 const mapStateToProps = state => {
     // debugger
@@ -12,7 +15,13 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    signup: user => dispatch(signup(user))
+    signup: user => dispatch(signup(user)),
+    closeModal: () => dispatch(closeModal()),
+    otherForm: (
+      <button onClick={() => dispatch(openModal('login'))}>
+        Signup
+      </button>
+    ),
   };
 };
 
