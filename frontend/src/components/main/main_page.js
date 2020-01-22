838a69652723058adf2519066dbcf2f465fdc298
Benjamin Huh <benjamin.s.huh@gmail.com>
2020-01-22 12:34:58 -0800

    Add video background to main page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -2,12 +2,19 @@
 
 import React from "react";
 import './main.scss';
+import background from "./background_video.mp4";
+
 
 class MainPage extends React.Component {
   render() {
     return (
-      <div className="main">
-        <h2>Welcome to ingredish</h2>
+      <div>
+        <video autoPlay muted loop id="background_video">
+          <source src={background} type="video/mp4" />
+        </video>
+        <div className="main">
+          <h2>Welcome to ingredish</h2>
+        </div>
       </div>
     );
   }
