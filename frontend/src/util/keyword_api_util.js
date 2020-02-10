4f07fb55363d449de045f1531eefabc6f3337e10
noel <noelseo@gmail.com>
2020-02-10 14:15:14 -0800

    Clean up codes for util and store

--- frontend/src/util/keyword_api_util.js
+++ frontend/src/util/keyword_api_util.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const createKeyword = (keyword) => {
-  return axios.post(`/api/keywords`, keyword);
+    return axios.post(`/api/keywords`, keyword);
 };
 
 export const fetchKeyword = (name) => {
